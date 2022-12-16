90400b502449904aee6a2898d5b2fb55f0f24730
CristianIvan00 <94533921+CristianIvan00@users.noreply.github.com>
2022-12-16 15:49:55 -0300

    cambio de api

--- src/servicios/noticias.js
+++ src/servicios/noticias.js
@@ -1,13 +1,14 @@
-const API_KEY = '048eba7ae357421ca2dc7ed6a9ebe6b9'
-const NEWS_API = 'https://newsapi.org/v2/everything?';
+const API_KEY = 'pub_14716623e1863bd5d1b7ae3208821c0f5dc1e';
+
+const NEWS_API = 'https://newsdata.io/api/1/news?';
 
 
 
 export const getListadoNoticias = async (criterioBusqueda, paginaActual) => {
     const respuesta = await fetch(`${NEWS_API}q=${criterioBusqueda}&apiKey=${API_KEY}&page=${paginaActual}&language=es`);
     const noticias =  respuesta.json();
-    console.log(respuesta.status)
-    return noticias
+    console.log(noticias.status)
+    return noticias;
     
 }
 
